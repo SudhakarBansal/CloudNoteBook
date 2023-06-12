f3bd80dac5ac1cc2b201c68de2e544aab1e93269
Sudhakar <sidhu1171bansal@gmail.com>
2023-06-12 11:31:33 +0530

    adding Login End point

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,7 +7,8 @@ const jwt = require('jsonwebtoken');            //to generate JWT token for secu
 
 const JWT_secret = "Thi$i$@$ecret";             //JWT secret key
 
-//cratea a user using : POST "api/auth/createuser" No Login required  
+//crate a user using : POST "api/auth/createuser" No Login required  
+
 router.post('/createuser', [
     body('name', 'Enter a valid name.').isLength({ min: 3 }),   //express Validators
     body('email', 'Enter a valid email.').isEmail(),
@@ -45,13 +46,52 @@ router.post('/createuser', [
             }
         };
         const authToken = jwt.sign(data, JWT_secret);
-        res.json(authToken);         //sending response
+        res.json({authToken});         //sending response
     }
 
     //catching any error if occurs...
     catch (error) {
         console.error(error.message);
-        res.status(500).send("Some Error occured...");
+        res.status(500).send("Internal Server Error : ");
+    }
+
+})
+
+//Authenticating a user using : POST "api/auth/login" No Login required  
+
+router.post('/login', [
+    body('email', 'Enter a valid email.').isEmail(),           //express Validators
+    body('password', 'Password cannot be blank.').exists()         //express Validators
+], async (req, res) => {
+    //if there are errors return Bad Request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: "Please try to login with correct credentials." });
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password);
+        if (!comparePassword) {
+            return res.status(400).json({ error: "Please try to login with correct credentials." });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+        const authToken = jwt.sign(payload, JWT_secret);
+        res.json({authToken});         //sending response
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error : ");
     }
 
 })
