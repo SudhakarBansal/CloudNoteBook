e59ecf7a691da30b1c966f85183191834d9ec844
Sudhakar <sidhu1171bansal@gmail.com>
2023-06-26 12:40:59 +0530

    creating Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is cloudnotebook</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
